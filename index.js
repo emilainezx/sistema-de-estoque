571d655591e4b5f0e853418312df3f295d9c9e80
emilainezx <emilaine125b@gmail.com>
2026-06-03 01:58:11 -0300

    feat: adiciona atualização de quantidade de produtos

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ function menu() {//FUNÇÂO DO MENU
   console.log("\nMENU");
   console.log("1 - Mostrar estoque");
   console.log("2 - Adicionar produto");
-  console.log("3 - Remover produto")
-  console.log("4 - Sair");
+  console.log("3 - Remover produto");
+  console.log("4 - Atualizar produto")
+  console.log("5 - Sair");
 
   rl.question("Qual opção? ", (opcao) => {
     console.log("Você digitou:", opcao)
@@ -36,10 +37,12 @@ function menu() {//FUNÇÂO DO MENU
     adicionarProduto();
   } else if (opcao == 3) {
     removerProduto();
-  } else if (opcao == 4) {
+  } else if(opcao == 4) {
+    atualizarProduto();
+  } else if (opcao == 5) {
     rl.close();
   } else {
-    console.log("Escolha uma das 4 opções");
+    console.log("Escolha uma das 5 opções");
     menu();
   }
   })
@@ -87,8 +90,11 @@ function removerProduto (){
         const produtoRemovido = estoque.splice(i, 1);
         encontrado = true
         console.log("Item", produtoRemovido[0].nome, "removido!");
+
         mostrarEstoque();
+
         menu();
+
         break;
       }
     }
@@ -96,5 +102,40 @@ function removerProduto (){
       console.log("Produto não encontrado");
       menu();
     }
+  })
+}
+
+function atualizarProduto() {
+  mostrarEstoque();
+  
+  rl.question("Qual produto deseja atualizar?", (produtoAtualizar) =>{
+    let encontrado = false;
+
+    for(let i = 0; i < estoque.length; i++){
+      if(estoque[i].nome === produtoAtualizar) {
+        encontrado = true;
+
+        rl.question("Qual a nova quantidade?", (quantidadeNova) =>{
+          const quantidadeNovaNumero = Number(quantidadeNova);
+
+          if(isNaN(quantidadeNovaNumero)) {
+            console.log("Quantidade informada não é um número")
+            menu();
+          }else {estoque[i].quantidade = quantidadeNovaNumero
+          console.log("Item:", estoque[i].nome, "alterado para a quantidade:", estoque[i].quantidade, "com sucesso!")
+
+          mostrarEstoque();
+
+          menu();
+          }
+        });
+
+      break;
+      }
+    }
+       if(!encontrado) {
+            console.log("Produto não encontrado");
+            menu();
+          }
   })
 }
