dc56469f47868dbc823741d56337401ee41289d3
emilainezx <emilaine125b@gmail.com>
2026-06-03 13:06:09 -0300

    feat: impede cadastro de produtos duplicados

--- index.js
+++ index.js
@@ -7,88 +7,106 @@ const rl = readline.createInterface({
 
 let estoque = [];
 
-function mostrarEstoque () {//FUNÇÂO PARA MOSTRAR ESTOQUE
+function mostrarEstoque() {
+  //FUNÇÂO PARA MOSTRAR ESTOQUE
 
-    console.log("\n === ESTOQUE ===");
+  console.log("\n === ESTOQUE ===");
 
-    for (let i = 0; i < estoque.length; i++) {
-    console.log(`Nome: ${estoque[i].nome} Quantidade: ${estoque[i].quantidade}`);
-    }
-    
+  for (let i = 0; i < estoque.length; i++) {
+    console.log(
+      `Nome: ${estoque[i].nome} Quantidade: ${estoque[i].quantidade}`,
+    );
+  }
 }
 
 console.log("\nSISTEMA DE ESTOQUE");
 menu();
 
-function menu() {//FUNÇÂO DO MENU
+function menu() {
+  //FUNÇÂO DO MENU
   console.log("\nMENU");
   console.log("1 - Mostrar estoque");
   console.log("2 - Adicionar produto");
   console.log("3 - Remover produto");
-  console.log("4 - Atualizar produto")
+  console.log("4 - Atualizar produto");
   console.log("5 - Sair");
 
   rl.question("Qual opção? ", (opcao) => {
-    console.log("Você digitou:", opcao)
+    console.log("Você digitou:", opcao);
     if (opcao == 1) {
-    mostrarEstoque();
-    menu();
-  } else if (opcao == 2) {
-    adicionarProduto();
-  } else if (opcao == 3) {
-    removerProduto();
-  } else if(opcao == 4) {
-    atualizarProduto();
-  } else if (opcao == 5) {
-    rl.close();
-  } else {
-    console.log("Escolha uma das 5 opções");
-    menu();
-  }
-  })
-}
-
-function adicionarProduto () {//FUNÇÂO PARA ADICIONAR PRODUTO
-rl.question("\nQual produto deseja cadastrar?", (produtoNovo) => {
-  console.log("\nVocê digitou", produtoNovo);
-
-  rl.question("\nQual a quantidade que tem desse produto?", (quantidadeNova) => {
-    console.log("\nVocê digitou:", quantidadeNova);
-    const quantidadeNovaNumero = Number(quantidadeNova);
-
-
-    if (isNaN(quantidadeNovaNumero)) {
-      console.log("Erro. digite um número!")
+      mostrarEstoque();
       menu();
-      return
+    } else if (opcao == 2) {
+      adicionarProduto();
+    } else if (opcao == 3) {
+      removerProduto();
+    } else if (opcao == 4) {
+      atualizarProduto();
+    } else if (opcao == 5) {
+      rl.close();
+    } else {
+      console.log("Escolha uma das 5 opções");
+      menu();
     }
-    const produto = {
-        nome: (produtoNovo),
-        quantidade: (quantidadeNovaNumero)
-    }
-    estoque.push(produto);
-
-    console.log(
-      `Produto adicionado: ${produtoNovo} quantidade: ${quantidadeNova}`,
-    );
-
-    console.log("\nEstoque atualizado:");
-
-    mostrarEstoque();
-    menu();
   });
-});
 }
 
-function removerProduto (){
+function adicionarProduto() {
+  //FUNÇÂO PARA ADICIONAR PRODUTO
+  rl.question("\nQual produto deseja cadastrar?", (produtoNovo) => {
+    console.log("\nVocê digitou", produtoNovo);
+
+    let produtoExistente = false;
+    for (let i = 0; i < estoque.length; i++) {
+      if (estoque[i].nome === produtoNovo) {
+        produtoExistente = true;
+        break;
+      }
+    }
+    if (produtoExistente) {
+      console.log("Produto já cadastrado!");
+      menu();
+    } else {
+      rl.question(
+        "\nQual a quantidade que tem desse produto?",
+        (quantidadeNova) => {
+          console.log("\nVocê digitou:", quantidadeNova);
+          const quantidadeNovaNumero = Number(quantidadeNova);
+
+          if (isNaN(quantidadeNovaNumero)) {
+            console.log("Erro. digite um número!");
+            menu();
+            return;
+          }
+          const produto = {
+            nome: produtoNovo,
+            quantidade: quantidadeNovaNumero,
+          };
+          estoque.push(produto);
+
+          console.log(
+            `Produto adicionado: ${produtoNovo} quantidade: ${quantidadeNova}`,
+          );
+
+          console.log("\nEstoque atualizado:");
+
+          mostrarEstoque();
+          menu();
+        },
+      );
+    }
+  });
+}
+
+function removerProduto() {
   mostrarEstoque();
-  rl.question("Qual produto você deseja remover?", (remover) =>{
+  rl.question("Qual produto você deseja remover?", (remover) => {
     let encontrado = false;
 
-    for(let i = 0; i < estoque.length; i++ ) {
-      if (estoque[i].nome === remover ) {
+    for (let i = 0; i < estoque.length; i++) {
+      if (estoque[i].nome === remover) {
         const produtoRemovido = estoque.splice(i, 1);
-        encontrado = true
+        encontrado = true;
         console.log("Item", produtoRemovido[0].nome, "removido!");
 
         mostrarEstoque();
@@ -102,40 +120,47 @@ function removerProduto (){
       console.log("Produto não encontrado");
       menu();
     }
-  })
+  });
 }
 
 function atualizarProduto() {
   mostrarEstoque();
-  
-  rl.question("Qual produto deseja atualizar?", (produtoAtualizar) =>{
+
+  rl.question("Qual produto deseja atualizar?", (produtoAtualizar) => {
     let encontrado = false;
 
-    for(let i = 0; i < estoque.length; i++){
-      if(estoque[i].nome === produtoAtualizar) {
+    for (let i = 0; i < estoque.length; i++) {
+      if (estoque[i].nome === produtoAtualizar) {
         encontrado = true;
 
-        rl.question("Qual a nova quantidade?", (quantidadeNova) =>{
+        rl.question("Qual a nova quantidade?", (quantidadeNova) => {
           const quantidadeNovaNumero = Number(quantidadeNova);
 
-          if(isNaN(quantidadeNovaNumero)) {
-            console.log("Quantidade informada não é um número")
+          if (isNaN(quantidadeNovaNumero)) {
+            console.log("Quantidade informada não é um número");
             menu();
-          }else {estoque[i].quantidade = quantidadeNovaNumero
-          console.log("Item:", estoque[i].nome, "alterado para a quantidade:", estoque[i].quantidade, "com sucesso!")
+          } else {
+            estoque[i].quantidade = quantidadeNovaNumero;
+            console.log(
+              "Item:",
+              estoque[i].nome,
+              "alterado para a quantidade:",
+              estoque[i].quantidade,
+              "com sucesso!",
+            );
 
-          mostrarEstoque();
+            mostrarEstoque();
 
-          menu();
+            menu();
           }
         });
 
-      break;
+        break;
       }
     }
-       if(!encontrado) {
-            console.log("Produto não encontrado");
-            menu();
-          }
-  })
+    if (!encontrado) {
+      console.log("Produto não encontrado");
+      menu();
+    }
+  });
 }
