1afd60cdf16dbca091fc14de990ffe901e1db2dc
Emilaine Bernardo <66503471+emilainezx@users.noreply.github.com>
2026-06-10 19:48:47 -0300

    feat: melhorar interface do terminal com boxen (#7)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import chalk from "chalk";
 import { menu } from "./menu.js";
+import boxen from "boxen";
 
-console.log(chalk.rgb(123, 45, 67).underline("SISTEMA DE ESTOQUE"));
+console.log(boxen(chalk.rgb(123, 45, 67).underline("SISTEMA DE ESTOQUE"), {padding: 1, margin: 1, borderStyle: 'classic'}));
+console.log();
 menu();
